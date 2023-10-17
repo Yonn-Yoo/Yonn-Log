0d37421dbcbf37e5a268e59e6e24f8d111405626
Yeonseung Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2023-10-17 21:17:34 +0900

    docs: initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        text1: '#ececec',
+        text2: '#d9d9d9',
+        text3: '#acacac',
+        text4: '#595959',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
