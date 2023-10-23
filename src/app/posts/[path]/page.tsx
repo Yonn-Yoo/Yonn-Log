97d50ad82f2228fab0de3c31f24a94cd674640a6
Yeonseung Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2023-10-23 21:56:41 +0900

    refactor: remove unnecessary file

--- src/app/posts/[path]/page.tsx
+++ src/app/posts/[path]/page.tsx
@@ -1,6 +1,6 @@
 import MarkdownViewer from '@/components/lib/markdown-viewer';
 import PostContent from '@/components/posts/post-content';
-import PostNavigation from '@/components/posts/post-navigation';
+import PostNavigator from '@/components/posts/post-navigator';
 import { getPostData } from '@/service/posts';
 
 type Props = {
@@ -9,12 +9,16 @@ type Props = {
 
 export default async function PostDetailPage({ params: { path } }: Props) {
   const post = await getPostData(path);
+  const { prevPost, nextPost } = post;
 
   return (
     <article className="max-w-4xl w-full mx-auto mt-5 md:mt-8 xl:mt-14 px-5">
       <PostContent post={post} />
       <MarkdownViewer content={post.content} />
-      <PostNavigation post={post} />
+      <section className="bg-background2 flex max-sm:flex-col h-32 mt-5">
+        {prevPost && <PostNavigator post={prevPost} isPrev />}
+        {nextPost && <PostNavigator post={nextPost} isPrev={false} />}
+      </section>
     </article>
   );
 }
