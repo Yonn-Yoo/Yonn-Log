93607e09666080cea1618bc4fdf898b338d6b33d
Yeonseung Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2023-10-23 13:13:07 +0900

    style: apply style for scroll bar and mobile post detail page

--- src/app/posts/[path]/page.tsx
+++ src/app/posts/[path]/page.tsx
@@ -12,16 +12,18 @@ export default async function PostDetailPage({ params: { path } }: Props) {
   const { title, image, date, category, content } = await getPostData(path);
 
   return (
-    <article className="max-w-4xl w-full mx-auto md:mt-14">
-      <div className="flex flex-col space-y-5">
-        <h1 className="font-bold xl:text-4xl text-text1">{title}</h1>
+    <article className="max-w-4xl w-full mx-auto mt-5 md:mt-8 xl:mt-14 px-5">
+      <div className="flex flex-col space-y-3 md:space-y-5 mb-5">
+        <h1 className="font-bold text-2xl md:text-3xl xl:text-4xl text-text1">
+          {title}
+        </h1>
         <div className="flex items-center space-x-1">
           <CalendarIcon />
-          <time className="text-text3">
+          <time className="max-md:text-sm text-text3">
             {moment(date).format('YYYY년 MM월 DD일')}
           </time>
         </div>
-        <span className="px-3 py-1 rounded-full bg-teal-600 w-fit text-sm">
+        <span className="max-md:text-sm px-2 md:px-3 py-1 rounded-full bg-teal-600 w-fit text-sm">
           {category}
         </span>
         <Image
@@ -29,7 +31,7 @@ export default async function PostDetailPage({ params: { path } }: Props) {
           alt="thumbnail"
           width={600}
           height={200}
-          className="w-full h-1/5 max-h-[400px] mx-auto object-cover object-center"
+          className="w-full h-1/5 max-h-52 md:max-h-[400px] mx-auto object-cover object-center"
         />
       </div>
       <MarkdownViewer content={content} />
--- src/components/lib/markdown-viewer.tsx
+++ src/components/lib/markdown-viewer.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
